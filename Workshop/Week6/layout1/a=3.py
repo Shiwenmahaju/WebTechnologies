6993066e026ccbe53148ca9995182535e47364e9
Shiwen <np03cs4a220393@heraldcollege.edu.np>
2023-02-11 12:39:37 +0545

    week12

---
+++ Workshop/Week6/layout1/a=3.py
@@ -0,0 +1,8 @@
+a=3
+b=1
+try:
+    d = a/b
+except ZeroDivisionError:
+    print("Can not be divided by zero")
+
+print("Rest of the code block")
---
+++ Workshop/Week6/layout1/classA.py
@@ -0,0 +1,6 @@
+class A:
+    def__init__(self,a,b):
+     self.a=a 
+     self.b=b
+obj=A(10,20)
+print(obj.a,obj.b)
